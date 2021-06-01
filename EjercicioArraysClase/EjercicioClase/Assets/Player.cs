e4f8c40af4504332f0c2965a421ed3cc1b9a44d6
VastoGrim <65916224+VastoGrim@users.noreply.github.com>
2021-06-01 12:48:58 -0500

    Scripts de Arrays
    Script de las bolas, script del player.

---
+++ EjercicioArraysClase/EjercicioClase/Assets/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+
+
+{
+    private Rigidbody rb;
+    public float jumpspeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey("left"))
+        {
+            rb.velocity = new Vector3(-jumpspeed, 0.0f);
+        }
+        else if (Input.GetKey("right"))
+        {
+            rb.velocity = new Vector3(jumpspeed, 0.0f);
+        }
+        else
+        {
+            rb.velocity = new Vector3(0.0f, 0.0f);
+        }
+    }
+}
---
+++ EjercicioArraysClase/EjercicioClase/Assets/ejemplosArray.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class ejemplosArray : MonoBehaviour
+{
+    public GameObject[] ballsarray;
+    public List<GameObject> ballscatch;
+    private bool catchball = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+           GameObject a =  Instantiate(ballsarray[Random.Range(0, ballsarray.Length)], new Vector3(Random.Range(-5, 5),Random.Range(-10,10)), Quaternion.identity);
+            ballscatch.Add(a);
+        }
+    }
+}
